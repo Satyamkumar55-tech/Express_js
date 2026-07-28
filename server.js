0db285146d0a689ce51fcd65cdab2ed455c5358f
Satyamkumar Singh <satyamranjeet2006@gmail.com>
2026-07-28 06:49:34 +0530

    Learned middleware, added one global middleware at the top

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req,res,next) => {
+    console.log("Request recived.......");
+    next();
+});
+
 app.get("/", (req,res) =>{
     res.send("This is the homepage");
 });
+
 
 app.get("/about", (req,res) =>{
     res.send("This is the about section");
